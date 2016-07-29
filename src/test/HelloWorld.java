e8071644d9e2fd69005afb8cabe96259cda6d235
Chenjie Wei <weichenjie2015@gmail.com>
2016-07-29 00:34:45 -0700

    LongestPalindromicSubstring LongestSubstringWithoutRepeatingCharacters MedianOfTwoSortedArrays

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,7 +2,15 @@ package test;
 
 public class HelloWorld {
 
-  public static void main(String args[]) {
-    System.out.println("Hello world!");
-  }
+	public static void main(String args[]) {
+		int temp = 1;
+		System.out.println("Hello world!");
+		modify(temp);
+		System.out.println(temp);
+	}
+
+	private static void modify(int temp) {
+		temp++;
+		System.out.println("Inside method: " + temp);
+	}
 }
